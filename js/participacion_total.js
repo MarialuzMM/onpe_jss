8172e54c5baec8d224f4e6e6d60dbb01402909b4
marialuz <marialuzmichi54@gmail.com>
2025-09-26 19:42:25 -0500

    ONPE

--- js/participacion_total.js
+++
@@ -1,8 +0,0 @@
-const getParticipacion_total = async (params) => {
-    const id=(new URLSearchParams(window.location.search)).get(`id`)
-    const data=await fetch(``)
-    
-
-}
-
-getParticipacion_total()
